728b5b4978aaa6708179ecedf94864bc80b2c642
Matthew Di Micelli <mrd2689a@gmail.com>
2021-09-10 17:48:12 -0400

    work on styles for homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,16 +5,18 @@ function Homepage(props) {
     return (
         <div>
             <NavBar />
-            <div>
-                <img src={forrestGumpPic} alt="Forrest Gump's face" />
-                <p>"Anyway, like I was sayin', shrimp is the fruit of the sea. 
-                You can barbecue it, boil it, broil it, bake it, saute it.
-                There's uh, shrimp-kabobs, shrimp creole, shrimp gumbo.
-                Pan fried, deep fried, stir-fried. There's pineapple shrimp, 
-                lemon shrimp, coconut shrimp, pepper shrimp, shrimp soup, 
-                shrimp stew, shrimp salad, shrimp and potatoes, shrimp burger, 
-                shrimp sandwich. That- that's about it."</p>
-                <button>Buy Shrimps</button>
+            <div className='homepage-body'>
+                <img className='forrest-pic' src={forrestGumpPic} alt="Forrest Gump's face" />
+                <div className='about-buybtn-container'>
+                    <p>"Anyway, like I was sayin', shrimp is the fruit of the sea. 
+                    You can barbecue it, boil it, broil it, bake it, saute it.
+                    There's uh, shrimp-kabobs, shrimp creole, shrimp gumbo.
+                    Pan fried, deep fried, stir-fried. There's pineapple shrimp, 
+                    lemon shrimp, coconut shrimp, pepper shrimp, shrimp soup, 
+                    shrimp stew, shrimp salad, shrimp and potatoes, shrimp burger, 
+                    shrimp sandwich. That- that's about it."</p>
+                    <button>Buy Shrimps</button>
+                </div>
             </div>
         </div>
     )
