6e627b7bd6eb4379a0b4b05c5003c5621b6ea1c8
Matt Di Micelli <matthewdimicelli@gmail.com>
2021-09-09 23:03:05 -0400

    put routes into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { ReactRouter, Switch, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import Shop from './Shop';
+import Homepage from './components/Homepage';
+import Shop from './components/Shop';
 
 function App() {
   return (
     <ReactRouter>
       <Switch>
-        <Route path='/' component={Homepage} />
-        <Route path='/shop' component={Shop} />
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={Shop} />
       </Switch>
     </ReactRouter>
   );
